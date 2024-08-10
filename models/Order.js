01f1201157feff3f0ae427df1fb5acc14241bc50
hardik1740 <hardikpandit1740@gmail.com>
2024-08-10 20:41:07 +0530

    Changes

---
+++ models/Order.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const OrderSchema = new mongoose.Schema({
+  products: [
+    {
+      _id: { type: String, required: true },
+      name: { type: String, required: true },
+      price: { type: Number, required: true },
+      image: { type: String },
+      rating: { type: Number },
+      reviewCount: { type: Number },
+      description: { type: String },
+      brand: { type: String },
+      originalPrice: { type: Number },
+      discountPercentage: { type: Number },
+      quantity: { type: Number, required: true },
+      category: { type: String },
+      stock: { type: Number },
+    }
+  ],
+  addresses: [ 
+    {
+      _id: { type: String },
+      name: { type: String, required: true },
+      email:{type:String},
+      room: { type: String },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String },
+      country: { type: String, required: true },
+      pin: { type: String, required: true },
+      phone: { type: String, required: true }
+    }
+  ],
+  discount: { type: Number },
+  total: { type: Number, required: true },
+  coupon: { type: String },
+  paymentMethod: { type: String, required: true },
+  images: [String],
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Order = mongoose.model('Order', OrderSchema);
+
+export default Order;
